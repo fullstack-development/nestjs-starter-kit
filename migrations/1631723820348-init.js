9a361d22ba0fd696909672bf30f6c41d6f56bff0
hersveit <cherkasov.ivan@yahoo.com>
2021-09-15 19:39:41 +0300

    typeorm migrations

---
+++ migrations/1631723820348-init.js
@@ -0,0 +1,8 @@
+const { MigrationInterface, QueryRunner } = require('typeorm');
+
+// migrations docs https://github.com/typeorm/typeorm/blob/master/docs/migrations.md
+module.exports = class init1631723820348 {
+    async up(queryRunner) {}
+
+    async down(queryRunner) {}
+};
