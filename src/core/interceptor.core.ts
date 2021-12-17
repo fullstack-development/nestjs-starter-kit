eacde8b853b5b8ab2efc587de47a48d55d4054bb
Daniil Petrochenkov <daniil.petro3@gmail.com>
2021-12-17 17:10:07 +0500

    Revert "fix server timeout error"
    This reverts commit 4195b8a19494ebabec17ca5a3c743e6628975fc8.

--- src/core/interceptor.core.ts
+++ src/core/interceptor.core.ts
@@ -14,13 +14,13 @@ export class HttpInterceptor implements NestInterceptor {
         context: ExecutionContext,
         next: CallHandler<unknown>,
     ): Promise<Observable<unknown>> {
+        const ctx = RequestContext.get<TransactionsContext>();
+        await ctx.transactions.start();
         const response: Response = context.switchToHttp().getResponse();
 
         return next.handle().pipe(
             mergeMap(async (data) => {
                 if (data instanceof ControllerResponse) {
-                    const ctx = RequestContext.get<TransactionsContext>();
-                    await ctx.transactions.start();
                     data.status ? await ctx.transactions.commit() : await ctx.transactions.abort();
                     response.status(data.status);
                     await ctx.transactions.stop();
