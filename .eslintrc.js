60baf4ace1193f78bfcf87bcaeec6d281ddd38a9
hersveit <cherkasov.ivan@yahoo.com>
2022-06-29 17:20:34 +0300

    fix eslint; fix prettier; format line endings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint/eslint-plugin', 'deprecation'],
     parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint/eslint-plugin', 'deprecation', 'nestjs-kit'],
     extends: [
         'plugin:@typescript-eslint/recommended',
         'prettier/@typescript-eslint',
@@ -27,7 +27,6 @@ module.exports = {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/array-type': [
             'error',
             {
