1a5e2d9361cdf5e3189f91bde3fa6772ee3ba45e
hersveit <cherkasov.ivan@yahoo.com>
2022-07-02 02:47:59 +0300

    remove log options

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        logger: ['error', 'warn', 'log', 'verbose', 'debug'],
-    });
+    const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
 
     await app.listen(3000);
